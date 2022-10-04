95b9c5d690d21316f686866e16dec9d2b2ba905e
Nurbek <nibragimov228@gmail.com>
2022-10-04 11:17:23 +0500

    one more try

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 //jshint esversion:6
 
 const express = require("express");
-const PORT = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 var random = require('mongoose-simple-random');
@@ -80,6 +79,11 @@ app.route("/test")
   }}
 })
 
-app.listen(PORT, function() {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000
+}
+
+app.listen(port, function() {
   console.log(`Server started on port 3000`);
 });
