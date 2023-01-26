843fb657aa285035bd5ab8c2ae5aa0ad7701f214
Nurbek <nibragimov228@gmail.com>
2023-01-26 13:20:40 +0500

    Initial com

--- app.js
+++ app.js
@@ -2,104 +2,127 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require('mongoose');
-var random = require('mongoose-simple-random');
+const mongoose = require("mongoose");
+var random = require("mongoose-simple-random");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
-
-mongoose.connect('mongodb+srv://ibragimovnd:7143316zN@cluster0.hrflc9n.mongodb.net/phraseDB', {useNewUrlParser: true});
-const phraseSchema = mongoose.Schema({
+mongoose.connect(
+  "mongodb+srv://ibragimovnd:7143316zN@cluster0.hrflc9n.mongodb.net/phraseDB",
+  { useNewUrlParser: true }
+);
+const krphraseSchema = mongoose.Schema({
   phrase: String,
   meaning: String,
-  reputation: { type: Number, default: 0 }
+  reputation: { type: Number, default: 0 },
 });
-phraseSchema.plugin(random);
+krphraseSchema.plugin(random);
 
-const Sample = mongoose.model("Sample", phraseSchema);
-const Phrase = mongoose.model("phrase", phraseSchema);
+const Sample = mongoose.model("Sample", krphraseSchema);
+const Krphrase = mongoose.model("krphrase", krphraseSchema);
 var progress = 1;
 var questionsArr = [];
-function reset(){
+function reset() {
   progress = 1;
 }
-app.route('/')
-.get(function(req,res){
-  res.render('list', {functs: reset()})
+app.route("/").get(function (req, res) {
+  res.render("list", { functs: reset() });
 });
 
-app.route('/add')
-.get(function(req, res){
-  res.render("add")
-})
-.post(function(req,res){
-  const newPhrase = new Phrase({
-    phrase: req.body.phrase,
-    meaning: req.body.meaning,
+app
+  .route("/add")
+  .get(function (req, res) {
+    res.render("add");
+  })
+  .post(function (req, res) {
+    const newKrphrase = new Krphrase({
+      phrase: req.body.krphrase,
+      meaning: req.body.meaning,
+    });
+    newKrphrase.save();
+    console.log("New Document has been saved");
+    res.redirect("/add");
   });
-  newPhrase.save()
-  console.log('New Document has been saved');
-  res.redirect("/add")
-});
 
-app.route("/test")
-.get(function(req, res){
-  function clicked(){
+app
+  .route("/test")
+  .get(function (req, res) {
+    function clicked() {}
+    var random = Math.floor(Math.random() * 4);
+    var question;
 
-  }
-  var random = Math.floor(Math.random() * 4);
-  var question;
-
-  Phrase.findRandom({}, {}, {limit: 4}, function(err, results) {
-    if (!err) {
-      randomData = JSON.parse(JSON.stringify(results[random]));
-      question = randomData.phrase;
-      reputation = randomData.reputation;
-      res.render("test", {randomQuest: question, reputation, results, progress})
-    }
-  });
-})
-.post(async function(req,res){
-  var selectedAnswer = req.body.button;
-    if(selectedAnswer === randomData.meaning){ // If answer is true
+    Krphrase.findRandom({}, {}, { limit: 4 }, function (err, results) {
+      if (!err) {
+        randomData = JSON.parse(JSON.stringify(results[random]));
+        question = randomData.phrase;
+        reputation = randomData.reputation;
+        res.render("test", {
+          randomQuest: question,
+          reputation,
+          results,
+          progress,
+        });
+      }
+    });
+  })
+  .post(async function (req, res) {
+    var selectedAnswer = req.body.button;
+    if (selectedAnswer === randomData.meaning) {
+      // If answer is true
       randomData.answer = "true";
-      Phrase.findOneAndUpdate({ _id: randomData._id }, { $inc: { reputation: 1 } }, {new: true },function(err, response) {
-        if (err) {
-        console.log(err);
-       }});
+      Krphrase.findOneAndUpdate(
+        { _id: randomData._id },
+        { $inc: { reputation: 1 } },
+        { new: true },
+        function (err, response) {
+          if (err) {
+            console.log(err);
+          }
+        }
+      );
       randomData.selected = selectedAnswer;
       questionsArr.push(randomData);
-    } else if(selectedAnswer !== randomData.meaning){ //If answer is wrong
+    } else if (selectedAnswer !== randomData.meaning) {
+      //If answer is wrong
       randomData.answer = "wrong";
-      Phrase.findOneAndUpdate({ _id: randomData._id }, { $inc: { reputation: -1 } }, {new: true },function(err, response) {
-        if (err) {
-        console.log(err);
-       }});
+      Krphrase.findOneAndUpdate(
+        { _id: randomData._id },
+        { $inc: { reputation: -1 } },
+        { new: true },
+        function (err, response) {
+          if (err) {
+            console.log(err);
+          }
+        }
+      );
       randomData.selected = selectedAnswer;
       questionsArr.push(randomData);
-    };
+    }
     if (progress === 10) {
       let sumOfAns = questionsArr.filter((el) => {
         return Object.values(el).some((val) =>
-          String(val).toLowerCase().includes("true"))})
-      res.render("Results", {result: questionsArr, sumOfAns});
+          String(val).toLowerCase().includes("true")
+        );
+      });
+      res.render("Results", { result: questionsArr, sumOfAns });
       progress = 1;
       questionsArr = [];
-    } else {res.redirect("/test")}
+    } else {
+      res.redirect("/test");
+    }
     progress++;
-})
+  });
 
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 3000
+  port = 3000;
 }
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Server started on port ${port}`);
 });
